832cc89d3cddc6c8f8c3fdbe410512bd7346de81
Jian Hui <jianhui.chan@honestbee.com>
2018-05-25 11:44:57 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import { Text, AppRegistry } from 'react-native';
+import React from 'react';
+import Header from './src/components/header';
+
+const App = () => (
+    <Header/>
+)
+
+
+AppRegistry.registerComponent('albums', () => App);
